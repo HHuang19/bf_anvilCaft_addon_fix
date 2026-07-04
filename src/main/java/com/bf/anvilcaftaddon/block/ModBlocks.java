396490ae39c0e93534fca6ea5d3018138cbaf1a8
haxu <148165763+HHuang19@users.noreply.github.com>
2026-07-04 01:13:14 -0500

    Add concise header comment to ModBlocks.java

--- src/main/java/com/bf/anvilcaftaddon/block/ModBlocks.java
+++ src/main/java/com/bf/anvilcaftaddon/block/ModBlocks.java
@@ -5,6 +5,7 @@ import com.bf.anvilcaftaddon.block.blocks.PowerBlock.PowerBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+// 模组方块的延迟注册器；调用 register(...) 将其绑定到 mod 事件总线以完成注册
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(AnvilCaftAddon.MODID);
     public static final DeferredHolder<net.minecraft.world.level.block.Block, PowerBlock> POWER_BLOCK
